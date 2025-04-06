c31554b0f1de74dfad7c5db3ed954e3d478adaf8
Petr John <xjohnp00@stud.fit.vutbr.cz>
2025-04-06 12:47:24 +0200

    Prepare for building

---
+++ src/index.ts
@@ -0,0 +1,20 @@
+import { Command } from './Command';
+import { ICommander } from './ICommander';
+import { IReferenceManager } from './IReferenceManager';
+import { IRepresentable } from './IRepresentable';
+import { Program } from './Program';
+import { ProgramRunner } from './ProgramRunner';
+import { ValueType } from './ValueType';
+import { ReferencedValue, ReferencedValueItemsAsObject } from './ReferencedValue';
+
+export {
+    Program,
+    Command,
+    ProgramRunner,
+    IReferenceManager,
+    IRepresentable,
+    ICommander,
+    ValueType,
+    ReferencedValue,
+    ReferencedValueItemsAsObject
+};
